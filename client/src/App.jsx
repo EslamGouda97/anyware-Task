af37e5be7fb1351bda80128795f2585e41212055
EslamGouda97 <106744433+EslamGouda97@users.noreply.github.com>
2023-10-28 11:54:27 +0200

    remove log

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,6 @@ import { useDispatch } from "react-redux";
 import jwt_decode from "jwt-decode";
 import { useEffect} from "react";
 import { fetchUser } from "./store/Slices/userSlice";
-import { fetchAnnouncement } from "./store/Slices/announcementSlice";
-import { fetchQuizzes } from "./store/Slices/quizzes";
-
 function App() {
   const dispatch = useDispatch();
 
@@ -14,10 +11,7 @@ function App() {
     const token = localStorage.getItem("token");
     if (token) {
       const decodedToken = jwt_decode(token);
-      // console.log("decodedToken", decodedToken);
       dispatch(fetchUser(decodedToken.userId));
-      dispatch(fetchAnnouncement());
-      dispatch(fetchQuizzes());
     }
   }, [dispatch]);
     return (
